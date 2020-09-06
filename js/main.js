d91c8e03d4d6ddbdb917f93a2de613ccada2e561
HeikiKotsar <heiki5000@gmail.com>
2020-09-06 23:12:46 +0300

    task comments fix, edit buttons, logout, container fix jne

--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
+const commentBtn = document.getElementById('commentBtn')
+const comments = document.getElementById('comments')
+const commentClose = document.getElementById('commentClose')
 
 
-function openSlideMenu() {
-    document.getElementById('side-menu').style.width = '250px';
+function openComments() {
+  comments.style.display = 'block'
 }
 
-function closeSlideMenu() {
-    document.getElementById('side-menu').style.width = '0';
+function closeComments() {
+  comments.style.display = 'none'
 }
 
+commentClose.addEventListener('click', closeComments)
+commentBtn.addEventListener('click', openComments)
 
 
-  const modal = document.querySelector('#my-modal');
-  const modalBtn = document.querySelector('#modal-btn');
-  const closeBtn = document.querySelector('.close');
-    
-    // Events
-    modalBtn.addEventListener('click', openModal);
-    closeBtn.addEventListener('click', closeModal);
-    window.addEventListener('click', outsideClick);
-    
-    // Open
-    function openModal() {
-      modal.style.display = 'block';
-    }
-    
-    // Close
-    function closeModal() {
-      modal.style.display = 'none';
-    }
-    
-    // Close If Outside Click
-    function outsideClick(e) {
-      if (e.target == modal) {
-        modal.style.display = 'none';
-      }
-  }
 
-  var container = document.getElementById("image-container");
-            function change_img(image) {
-                container.src = image.src;
-            } 
+// function closeSlideMenu() {
+//     document.getElementById('side-menu').style.width = '0';
+// }
+
+
+
+//   const modal = document.querySelector('#my-modal');
+//   const modalBtn = document.querySelector('#modal-btn');
+//   const closeBtn = document.querySelector('.close');
+    
+//     // Events
+//     modalBtn.addEventListener('click', openModal);
+//     closeBtn.addEventListener('click', closeModal);
+//     window.addEventListener('click', outsideClick);
+    
+//     // Open
+//     function openModal() {
+//       modal.style.display = 'block';
+//     }
+    
+//     // Close
+//     function closeModal() {
+//       modal.style.display = 'none';
+//     }
+    
+//     // Close If Outside Click
+//     function outsideClick(e) {
+//       if (e.target == modal) {
+//         modal.style.display = 'none';
+//       }
+//   }
+
+//   var container = document.getElementById("image-container");
+//             function change_img(image) {
+//                 container.src = image.src;
+//             } 
